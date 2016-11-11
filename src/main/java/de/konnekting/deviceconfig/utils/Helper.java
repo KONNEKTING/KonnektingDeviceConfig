2087301b867abb8933bce41cccc413832d22062a
tuxedo0801 <info@root1.de>
2016-11-11 12:33:53 +0100

    fixed possible license issue with "hexToBytes" which has been taken from StackOverflow, which is actually CCSA3.0 licensed (?!). This implementation is from my point of view too trivial to license it. But to overcome any further discussions, code has been changed to an own implementation (might be "slower", but who cares?!)

--- src/main/java/de/konnekting/deviceconfig/utils/Helper.java
+++ src/main/java/de/konnekting/deviceconfig/utils/Helper.java
@@ -37,19 +37,18 @@ import org.slf4j.LoggerFactory;
 public class Helper {
 
     private static final Logger LOG = LoggerFactory.getLogger(Helper.class);
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
     private static Map<ParamType, Integer> PARAM_SIZE_MAP = new HashMap<>();
-    
+
     static {
         PARAM_SIZE_MAP.put(ParamType.INT_8, 1);
         PARAM_SIZE_MAP.put(ParamType.UINT_8, 1);
-        
+
         PARAM_SIZE_MAP.put(ParamType.INT_16, 2);
         PARAM_SIZE_MAP.put(ParamType.UINT_16, 2);
-        
+
         PARAM_SIZE_MAP.put(ParamType.INT_32, 4);
         PARAM_SIZE_MAP.put(ParamType.UINT_32, 4);
-        
+
         PARAM_SIZE_MAP.put(ParamType.RAW_1, 1);
         PARAM_SIZE_MAP.put(ParamType.RAW_2, 2);
         PARAM_SIZE_MAP.put(ParamType.RAW_3, 3);
@@ -61,7 +60,7 @@ public class Helper {
         PARAM_SIZE_MAP.put(ParamType.RAW_9, 9);
         PARAM_SIZE_MAP.put(ParamType.RAW_10, 10);
         PARAM_SIZE_MAP.put(ParamType.RAW_11, 11);
-        
+
         PARAM_SIZE_MAP.put(ParamType.STRING_11, 11);
     }
 
@@ -69,28 +68,20 @@ public class Helper {
         return bytesToHex(bytes, false);
     }
 
-    public static String bytesToHex(byte[] bytes, boolean withWhitespace) {
-        char[] hexChars = new char[bytes.length * 2 + (withWhitespace ? bytes.length : 0)];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            if (withWhitespace) {
-                hexChars[j * 2 + j] = HEX_ARRAY[v >>> 4];
-                hexChars[j * 2 + j + 1] = HEX_ARRAY[v & 0x0F];
-                hexChars[j * 2 + j + 2] = " ".charAt(0);
-            } else {
-                hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-                hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-            }
+    public static String bytesToHex(byte[] bytearray, boolean whitespace) {
+        StringBuilder sb = new StringBuilder(bytearray.length * 2);
+        for (int i = 0; i < bytearray.length; i++) {
+            sb.append(String.format(whitespace?"%02x ":"%02x", bytearray[i] & 0xff));
         }
-        return new String(hexChars);
+        return sb.toString().trim();
     }
 
     public static byte[] hexToBytes(String s) {
-        
-        if (!s.matches("^[0-9a-fA-F]+$") || s.length()%2!=0) {
-            throw new NumberFormatException("Given value '"+s+"' is not a valid hex string");
+
+        if (!s.matches("^[0-9a-fA-F]+$") || s.length() % 2 != 0) {
+            throw new NumberFormatException("Given value '" + s + "' is not a valid hex string");
         };
-        
+
         int len = s.length();
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
@@ -104,15 +95,6 @@ public class Helper {
     private static final Pattern PA_PARKED_PATTERN = Pattern.compile("\\A\\d{1,2}\\.\\d{1,2}\\.\\z");
     private static final Pattern GA_PATTERN = Pattern.compile("\\A\\d{1,2}/\\d{1,2}/\\d{1,3}\\z");
 
-//    public static byte[] addrToBytes(int a, int b, int c) {
-//        byte[] buffer = new byte[2];
-//
-//        buffer[0] = (byte) ((byte) (a << 4) | b);
-//        buffer[1] = (byte) c;
-//
-//        return buffer;
-//    }
-    
     public static byte[] convertGaToBytes(String ga) {
         try {
             return Utils.getGroupAddress(ga).toByteArray();
@@ -121,7 +103,7 @@ public class Helper {
             return null;
         }
     }
-    
+
     public static byte[] convertIaToBytes(String ia) {
         if (isParkedAddress(ia)) {
             LOG.error("Individual address is parked. Cannot convert");
@@ -166,10 +148,12 @@ public class Helper {
     }
 
     public static boolean checkValidGa(String ga) {
-        
+
         // allow empty GA --> unused ComObject
-        if (ga.isEmpty()) return true;
-        
+        if (ga.isEmpty()) {
+            return true;
+        }
+
         Matcher matcher = GA_PATTERN.matcher(ga);
         boolean found = false;
         while (matcher.find()) {
@@ -221,10 +205,10 @@ public class Helper {
         }
         return false;
     }
-    
+
     public static int getParameterSize(ParamType paramType) {
         if (!PARAM_SIZE_MAP.containsKey(paramType)) {
-            throw new RuntimeException("Someone forgot to add the size of "+paramType.name()+" to PARAM_SIZE_MAP!");
+            throw new RuntimeException("Someone forgot to add the size of " + paramType.name() + " to PARAM_SIZE_MAP!");
         }
         return PARAM_SIZE_MAP.get(paramType);
     }
@@ -242,7 +226,7 @@ public class Helper {
                 return false;
         }
     }
-    
+
     public static boolean isRawType(ParamType paramType) {
         switch (paramType) {
             case RAW_1:
@@ -261,18 +245,9 @@ public class Helper {
                 return false;
         }
     }
-    
-//    public static boolean isFloatType(ParameterType paramType) {
-//        switch (paramType) {
-//            case FLOAT32:
-//                return true;
-//            default:
-//                return false;
-//        }
-//    }
 
     public static String getTempFilename(String suffix) throws IOException {
-        File createTempFile = File.createTempFile("KONNEKTING", "Temp"+suffix);
+        File createTempFile = File.createTempFile("KONNEKTING_", "_Temp" + suffix);
         createTempFile.delete();
         createTempFile.deleteOnExit();
         return createTempFile.getName();
